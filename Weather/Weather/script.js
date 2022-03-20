6cb6b8c0654e57f6237a51892cd80d3bf155764b
Sacesha <saceshabennett81@gmail.com>
2022-03-19 22:12:23 -0400

    test

--- Weather/Weather/script.js
+++ Weather/Weather/script.js
@@ -1,3 +1,7 @@
-function alert 
+function alert() 
 // displays a page alert when any of 3 buttons are clicked
 // take in 3 pieces of info from .cities document.querySelector()
+
+{
+    alert('');
+}
